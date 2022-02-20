b77a88e28f3594fe09b3e1de7a45080ab58695cd
Andrew Yushkevich <tenayu@ten.Tenna.com>
2022-02-20 15:08:28 +0300

    removed frontend fix

--- client/src/features/CreateUpdateAccount/CreateAccount.jsx
+++ client/src/features/CreateUpdateAccount/CreateAccount.jsx
@@ -2,11 +2,11 @@ import { useAccountFormOptions } from "../../hooks/useAccountFormOptions";
 import { accountService } from "../../services/accountService";
 import { CreateUpdateAccount } from "./CreateUpdateAccount";
 import { toast } from "react-toastify";
-import * as dayjs from "dayjs";
+// import * as dayjs from "dayjs";
 
 export const CreateAccount = () => {
   const onSubmit = (data) => {
-    data.EndDate = dayjs(data.EndDate).subtract(1, "day").toISOString();
+    // data.EndDate = dayjs(data.EndDate).subtract(1, "day").toISOString();
     const condition = data.AccountTypeName.value
       .toLowerCase()
       .includes("deposit");
